c955caabbea4f73cce295f2e26c2e202699472bb
alfardan59 <alfardanarfin500@gmail.com>
2025-01-23 12:52:58 +0530

    Login and Regitser Integration Done

---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../context/user.context'
+
+const Home = () => {
+
+    const {user}=useContext(UserContext)
+
+  return (
+    <div>
+      {user}
+    </div>
+  )
+}
+
+export default Home
